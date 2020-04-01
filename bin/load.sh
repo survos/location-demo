3fdb05240efa021ec57358530e47415e4d0a20db
Tac Tacelosky <tacman@gmail.com>
2020-04-01 14:34:24 -0400

    load USA

--- bin/load.sh
+++ bin/load.sh
@@ -1,4 +1,5 @@
+rm var/data.db
 bin/console doctrine:schema:update --force
 bin/console doctrine:fixtures:load -n
-bin/console bordeux:geoname:import -a http://download.geonames.org/export/dump/CA.zip
+bin/console bordeux:geoname:import -a http://download.geonames.org/export/dump/US.zip
 
