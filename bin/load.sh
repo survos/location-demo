4aaecd93c7f694d6e4fbcf1003d7db52dc52c118
Tac Tacelosky <tacman@gmail.com>
2021-11-11 10:51:37 -0500

    move to bundle

--- bin/load.sh
+++ bin/load.sh
@@ -1,7 +1,7 @@
 rm geonames.db
 #bin/console doctrine:database:drop --force --em=geonames
 bin/console doctrine:schema:update --force --dump-sql
-bin/console doctrine:fixtures:load -n
+bin/console survos:location:load
 # bin/console bordeux:geoname:import -a http://download.geonames.org/export/dump/US.zip --timezones=0 --download-dir /home/tac/data/bordeux/geoname --env=prod
 #bin/console bordeux:geoname:import -a http://download.geonames.org/export/dump/MX.zip  --timezones=0 --download-dir /home/tac/data/bordeux/geoname -vvv
 
