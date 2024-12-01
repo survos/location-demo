6788b9e3e00c216a08403e39c28172382c5a0d2d
Tac Tacelosky <tacman@gmail.com>
2024-12-01 17:06:18 -0500

    partial transition to using bundle and symfony 6

--- rector.php
+++ rector.php
@@ -1,72 +1,44 @@
 <?php
+
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Rector\Php80\Rector\Class_\DoctrineAnnotationClassToAttributeRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\Symfony\Symfony61\Rector\Class_\CommandPropertyToAttributeRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 
-use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
-use Rector\Php80\ValueObject\AnnotationToAttribute;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+return RectorConfig::configure()
+    ->withPaths([
+//        __DIR__ . '/assets',
+//        __DIR__ . '/config',
+//        __DIR__ . '/public',
+        __DIR__ . '/src',
+//        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        DoctrineSetList::GEDMO_ANNOTATIONS_TO_ATTRIBUTES,
 
-use Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector;
-use Rector\Arguments\ValueObject\ReplaceFuncCallArgumentDefaultValue;
+    ])
+    ->withConfiguredRule(
+        \Rector\Php80\Rector\Class_\AnnotationToAttributeRector::class,
+        [
+            new \Rector\Php80\ValueObject\AnnotationToAttribute('ApiPlatform\Metadata\ApiFilter'),
+            new \Rector\Php80\ValueObject\AnnotationToAttribute('ApiPlatform\Metadata\ApiResource'),
+            new \Rector\Php80\ValueObject\AnnotationToAttribute('ApiPlatform\Metadata\ApiProperty'),
+            new \Rector\Php80\ValueObject\AnnotationToAttribute('ApiPlatform\Metadata\ApiSubresource'),
+        ])
 
-use Rector\Symfony\Rector\Class_\ChangeFileLoaderInExtensionAndKernelRector;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-
-    // paths to refactor; solid alternative to CLI arguments
-    // $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-
-    // is your PHP version different from the one your refactor to? [default: your PHP version], uses PHP_VERSION_ID format
-     $parameters->set(Option::PHP_VERSION_FEATURES, \Rector\Core\ValueObject\PhpVersion::PHP_80);
-
-    // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
-//    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
-
-    // here we can define, what sets of rules will be applied
-    // tip: use "SetList" class to autocomplete sets
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_80);
-//    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-
-    // register single rule
-    $services = $containerConfigurator->services();
-
-    $services->set(TypedPropertyRector::class)
-        ->call('configure', [[
-            TypedPropertyRector::CLASS_LIKE_TYPE_ONLY => false,
-        ]]);
-
-    $services->set(AnnotationToAttributeRector::class)
-        ->call('configure', [[
-            AnnotationToAttributeRector::ANNOTATION_TO_ATTRIBUTE => ValueObjectInliner::inline([
-                new AnnotationToAttribute('Symfony\Component\Routing\Annotation\Route'),
-            ]),
-        ]]);
-
-    $services->set(ChangeFileLoaderInExtensionAndKernelRector::class)
-        ->call('configure', [[
-            ChangeFileLoaderInExtensionAndKernelRector::FROM => 'yaml',
-            ChangeFileLoaderInExtensionAndKernelRector::TO => 'php',
-        ]]);
-
-    $services->set(DoctrineAnnotationClassToAttributeRector::class)
-        ->call('configure', [[
-            DoctrineAnnotationClassToAttributeRector::REMOVE_ANNOTATIONS => true,
-        ]]);
-
-    $services->set(FunctionArgumentDefaultValueReplacerRector::class)
-        ->call('configure', [[
-            FunctionArgumentDefaultValueReplacerRector::REPLACED_ARGUMENTS => ValueObjectInliner::inline([
-                new ReplaceFuncCallArgumentDefaultValue('version_compare', 2, 'gte', 'ge'),
-            ]),
-        ]]);
-};
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+        CommandPropertyToAttributeRector::class,
+        InlineConstructorDefaultToPropertyRector::class,
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class
+    ]);
